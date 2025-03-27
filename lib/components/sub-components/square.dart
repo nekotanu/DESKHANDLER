001feeb7301b3b3388348d57391496b7f72ef141
nekotanu <ayutanu1230@gmail.com>
2025-03-27 11:26:10 +0530

    comp-subfolder

---
+++ lib/components/sub-components/square.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Square extends StatelessWidget {
+  const Square({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
