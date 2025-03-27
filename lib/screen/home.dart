db7819caac7f09f466b5570017585e9bae20cae8
nekotanu <ayutanu1230@gmail.com>
2025-03-27 10:44:20 +0530

    inital commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCalendar(view: CalendarView.month);
+  }
+}
